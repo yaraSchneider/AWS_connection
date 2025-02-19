c77c50fd85bd1ae721701eb2bd1378f258eb59b4
yaraSchneider <yara_schneider@estudante.sesisenai.org.br>
2025-02-18 21:47:01 -0300

    Apagando chaves secretas

--- Config/awsConfig.js
+++ Config/awsConfig.js
@@ -2,8 +2,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: 'us-east-1',  // Substitua pela sua região
-  accessKeyId: 'AKIA5RRHCKYZZ7ADAU6V',
-  secretAccessKey: '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'
+  accessKeyId: '',
+  secretAccessKey: ''
 });
 
 const s3 = new AWS.S3();
