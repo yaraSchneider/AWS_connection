54f82882094f631d46dda81041cd127532ab0615
yaraSchneider <yara_schneider@estudante.sesisenai.org.br>
2025-03-04 19:45:30 -0300

    Comentario

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost', // Altere para o host do seu banco de dados
-  user: 'root', // Altere para seu usuário do banco
-  password: '', // Altere para sua senha
-  database: 'upload_db', // Altere para o nome do seu banco de dados
+  host: 'localhost', 
+  user: 'root', 
+  password: '', 
+  database: 'upload_db', 
 });
 
 module.exports = pool;
